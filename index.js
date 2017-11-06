404c83f71e716cf123dd092543620776df756652
PJ Frias <peejfrias@gmail.com>
2017-11-06 22:45:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return newObj
 }
 
-function destructivelyUpdatedriverWithKeyAndValue(driver, key, value) {
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver.key = value
 }
